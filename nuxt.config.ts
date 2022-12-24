ad43de47a478535d3a534facdcbb523230be8c36
CRBroughton <crbroughton@posteo.uk>
2022-12-24 10:07:23 +0000

    feat: :label: add type checking to build command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+    typeCheck: 'build',
+  },
   build: { transpile: ['trpc-nuxt'] },
   css: ['~/assets/css/main.css'],
   postcss: {
