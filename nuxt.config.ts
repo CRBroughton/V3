4ede4b1b03227de69d0b8a5457b6088adc4aa0dd
CRBroughton <crbroughton@posteo.uk>
2023-03-10 19:25:57 +0000

    feat: :sparkles: add experiment component island support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  experimental: {
+    componentIslands: false,
+  },
   auth: {
     origin: process.env.AUTH_ORIGIN,
   },
