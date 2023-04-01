5098e9abfb3c9f8377f4fee6a98381ed75554352
CRBroughton <crbroughton@posteo.uk>
2023-04-01 11:09:24 +0100

    chore: :heavy_plus_sign: swap tailwindcss for nuxt module

---
+++ .changeset/poor-trees-fry.md
@@ -0,0 +1,5 @@
+---
+"v3": patch
+---
+
+swap tailwindcss for nuxt module
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@sidebase/nuxt-auth', '@nuxt/devtools'],
+  modules: ['@sidebase/nuxt-auth', '@nuxt/devtools', '@nuxtjs/tailwindcss'],
   build: { transpile: ['trpc-nuxt'] },
   css: ['~/assets/css/main.css'],
   postcss: {
